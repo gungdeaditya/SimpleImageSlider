e1cc8e143427c5b1a4f74d90648ee3c935e34c45
gungdeaditya <gunkdep@gmail.com>
2017-05-05 16:19:21 +0700

    initial commit

---
+++ app/src/main/java/com/projects/gungde/sampleimageslide/MainActivity.java
@@ -0,0 +1,48 @@
+package com.projects.gungde.sampleimageslide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.daimajia.slider.library.Indicators.PagerIndicator;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SliderLayout slideShow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        slideShow = (SliderLayout) findViewById(R.id.slider);
+        slideShow.setCustomIndicator((PagerIndicator) findViewById(R.id.custom_indicator));
+
+        HashMap<String,String> url_maps = new HashMap<String, String>();
+        url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
+        url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
+        url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
+        url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
+
+        for(String name : url_maps.keySet()){
+            TextSliderView textSliderView = new TextSliderView(this);
+            // initialize a SliderLayout
+            textSliderView
+                    .description(name)
+                    .image(url_maps.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit);
+
+            //add your extra information
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle()
+                    .putString("extra",name);
+
+            slideShow.addSlider(textSliderView);
+        }
+        slideShow.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+    }
+}
